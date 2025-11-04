61471f7e9e3e998949f6320354f040635c6eae00
Jarray Thamer <thamer99jarray@gmail.com>
2025-11-04 15:42:16 +0100

    removed description in education

--- data/education.tsx
+++ data/education.tsx
@@ -4,33 +4,7 @@ const educations = () => {
       title: "Software Development Bootcamp",
       company: "GoMyCode",
       info: "Kairouan, Tunis",
-      description: (
-        <ul className="list-disc pl-5 space-y-2">
-          <li>
-            Founded and developed <strong>WeBloom Studio</strong>, a creative
-            web design and development brand helping businesses build modern,
-            trustworthy online identities.
-          </li>
-          <li>
-            Design and build responsive websites using{" "}
-            <strong>React, Next.js, and TailwindCSS</strong> with a focus on
-            performance, clarity, and user experience.
-          </li>
-          <li>
-            Lead every step of the process — from client discovery and UI/UX
-            design in Figma to front-end implementation and deployment.
-          </li>
-          <li>
-            Collaborate with local businesses to create{" "}
-            <strong>clean, mobile-first, and conversion-focused</strong> web
-            solutions that reflect their brand identity.
-          </li>
-          <li>
-            Integrate essential tools such as WhatsApp contact systems, Google
-            Maps, and SEO optimization to increase client visibility and trust.
-          </li>
-        </ul>
-      ),
+      description: <ul className="list-disc pl-5 space-y-2"></ul>,
       date: "2025 - 2025",
       current: false,
     },
@@ -39,16 +13,7 @@ const educations = () => {
         "National License in Science and Technology – Information Systems Engineering",
       company: "Higher Institute of Applied Sciences and Technology",
       info: "Kairouan, Tunis",
-      description: (
-        <p>
-          Designed and developed responsive websites for various international
-          clients, delivering modern and conversion-focused solutions. One
-          notable project was , a Dubai-based visa consultancy landing page
-          built to streamline service inquiries through an integrated request
-          form. Focused on clean UI, smooth performance, and optimized lead
-          generation to enhance user experience.
-        </p>
-      ),
+      description: <p></p>,
       date: "2021 - 2024",
       current: false,
     },
