be89b6ebd5fd4bff529c55305dc4c35ff302fc8d
Jarray Thamer <thamer99jarray@gmail.com>
2025-11-04 12:37:13 +0100

    commented project section

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Hero />
       <HomeTechStack />
       <WorkHistory />
-      {//<Projects />}
+      {/*<Projects />*/}
       <HomeSocials />
       <HomeContact />
     </div>
